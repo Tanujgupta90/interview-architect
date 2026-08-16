e39e96a6ffb8cbffb7de932efdc1e34e01ce423d
Tanuj Gupta <tanujgupta7426@gmail.com>
2026-08-16 17:45:35 +0530

    fix api url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,9 +1,10 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL: "https://interview-architect.onrender.com",
     withCredentials: true
-})
+});
+
 
 
 export async function register({ username, email, password }) {
